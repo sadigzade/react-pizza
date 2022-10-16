3c0ab439712c50c5e4d1ec27b1aaf632e8a19e92
Хикмет Садигзаде <sadigzade.hi@mail.ru>
2022-10-16 16:51:08 +0300

    adding layout and styles, connecting three components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Categories from './components/Categories/Categories';
+import Header from './components/Header/Header';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import Sort from './components/Sort/Sort';
+
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={525} />
+            <PizzaBlock title="Чизбургер-пицца" price={395} />
+            <PizzaBlock title="BBG" price={620} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
